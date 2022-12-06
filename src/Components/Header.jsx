43d9cbd6ff19fbded72f2e39378be120bfb91464
Rafael Seoanes <rafaseoanes9@gmail.com>
2022-12-05 22:13:52 -0500

    header updated with scroll arrow and hashtags

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,8 @@ function Header() {
         <h6 className='text-yellow-600 flex  justify-center'   >FOOD & DRINKS</h6>
         
     <div className='flex justify-center items-center'>
-        
+        <h5 className='text-green-900 rotate-[-90deg] absolute  left-8 custom-spacing '>#Bar #massai</h5>
+      
         <div className="max-w-xl md:mr-16 text-center md:text-left  " >
         <h1 className="text-yellow-500 text-4xl md:text-6xl " >BRUNCH,<br/> LUNCH<br/> & TEATIME</h1>
         <br />
@@ -23,6 +24,10 @@ function Header() {
         <img className="max-w-md" src='https://i.ibb.co/XJS2whd/New-Project.png' alt="Header" />
         </div>
   
+  </div>
+  <div className='absolute bottom-1 right-8'>
+    <img src="https://i.ibb.co/dPFzb3y/icons8-up-arrow-100.png" alt="arrow" />
+    <p className='text-yellow-500 text-center ' >Desliza</p>
   </div>
     </div>
   )
